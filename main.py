a8752c4d0d4bc217084b133de25ba2b5a90f380b
GabeFlores7 <gabe7.flores@gmail.com>
2025-08-31 19:24:20 -0500

    add user input feature

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
 
@@ -10,9 +11,14 @@ client = genai.Client(api_key=api_key)
 
 def main():
     print("Hello from building-ai-agent-project!")
+    args = sys.argv
+    if len(args) < 2:
+        print("Prompt required. Please input prompt for script to execute.")
+        sys.exit(1)
+
     response = client.models.generate_content(
     model='gemini-2.0-flash-001',
-    contents= "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    contents= args[1]
 
 )
     print(response.text)
