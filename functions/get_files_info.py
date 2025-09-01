233e3b99e5bec64944252fd3f7cd39f4e7d7fbf1
GabeFlores7 <gabe7.flores@gmail.com>
2025-09-01 17:02:10 -0500

    give LLM ability to describe functions and arguments it wants user to call

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,4 +1,7 @@
 import os
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
 
 def get_files_info(working_directory, directory="."):
     # Create variables to store absolute paths of working dir and desired dir
@@ -28,3 +31,20 @@ def get_files_info(working_directory, directory="."):
         return contents_string
     except Exception as e: # return error string if any errors occur
         return f"Error: {e}"
+
+
+# Used to tell the LLM how to use the function
+
+schema_get_files_info = types.FunctionDeclaration(
+    name="get_files_info",
+    description="Lists files in the specified directory along with their sizes, constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "directory": types.Schema(
+                type=types.Type.STRING,
+                description="The directory to list files from, relative to the working directory. If not provided, lists files in the working directory itself.",
+            ),
+        },
+    ),
+)
--- main.py
+++ main.py
@@ -3,32 +3,59 @@ import sys
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
-
+from functions.get_files_info import schema_get_files_info
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
 client = genai.Client(api_key=api_key)
+model_name = 'gemini-2.0-flash-001'
+
+system_prompt = """
+You are a helpful AI coding agent.
+
+When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+- List files and directories
+
+All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+"""
+
+available_functions = types.Tool(
+    function_declarations=[
+        schema_get_files_info,
+    ]
+)
 
 def main():
     print("Hello from building-ai-agent-project!") # user greeting
 
     args = sys.argv # recieve user input/prompt from CLI
-    user_prompt = args[1] # store user input/prompt
 
-    messages = [types.Content(role="user", parts=[types.Part(text=user_prompt)]),] # store prompt history into messages list of objects
-    
     if len(args) < 2: # If no prompt was passed, print error and exit script
         print("Prompt required. Please input prompt for script to execute.")
         sys.exit(1)
 
-    # Feed user input into LLM
-    response = client.models.generate_content(
-    model='gemini-2.0-flash-001',
-    contents= messages
+    user_prompt = args[1] # store user input/prompt
 
+    messages = [types.Content(role="user", parts=[types.Part(text=user_prompt)]),] # store prompt history into messages list of objects
+    
+
+    # Feed user input into LLM
+    response= client.models.generate_content(
+    model= model_name,
+    contents= messages,
+    config= types.GenerateContentConfig(
+        tools= [available_functions],
+        system_instruction= system_prompt),
 )
-    print(response.text) # print model's response to console
+
+
+    if response.function_calls:
+        for functions_call_part in response.function_calls:
+            print(f"Calling function: {functions_call_part.name}({functions_call_part.args})")
+    else:
+        print(response.text) # print model's response to console
 
     # If verbose option is selected
     if len(args) > 2 and args[2] == "--verbose":
